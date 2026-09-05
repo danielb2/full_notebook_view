bd81ee9ff4915e4400355a29a9997dc045623ee1
Daniel Bretoi <daniel@otherware.org>
2026-09-05 18:14:05 +0700

    fix: sync explorer with selected note

--- src/webview.js
+++ src/webview.js
@@ -1750,10 +1750,11 @@ webviewApi.onMessage(function (message) {
 		case 'noteSelected':
 			state.selectedNoteId = message.noteId;
 			state.selectedFolderId = message.folderId || state.selectedFolderId;
-			if (state.shouldExpandOnNextSelect && message.noteId) {
+			if (message.noteId) {
 				state.shouldExpandOnNextSelect = false;
+				if (state.activeTab !== 'notebooks') switchToTab('notebooks');
 				expandToNote(message.noteId, true);
-			} else if (!state.isSearchMode) {
+			} else {
 				renderTree();
 			}
 			break;
